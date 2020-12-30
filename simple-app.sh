3dcca3f180344f5b45e1e01aad5b676b40757f06
Hit The Target <36575353+tolek-rudnicki@users.noreply.github.com>
2020-12-30 12:59:37 +0100

    Create simple-app.sh

---
+++ simple-app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'Please insert the code from README.md and type in the sudo password'
+cd /bin && sudo gedit simple-app
+echo 'The App is being created, you should be able to type "simple-app" into your terminal'
